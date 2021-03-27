f2e6c6b7be3a860e2494659785f751e255ccdfcc
Nishant <nishantb1130@gmail.com>
2021-03-27 16:54:26 -0700

    base

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
 
 export const Navbar = () => {
   return(
-    <div>
-        <img src="../../../assets/logo.png"/>
-        <div>
-          
-        </div>
-    </div>
+    <Navbar bg="FFFFFF">
+      <Navbar.brand href="/">VoTeX</Navbar.brand>
+      <Nav>
+        <Nav.Link href="">Login</Nav.Link> 
+        <Nav.Link href="">New Doc</Nav.Link>
+        <Nav.Link href="">Name</Nav.Link>
+      </Nav>
+    </Navbar>
   )
 }
