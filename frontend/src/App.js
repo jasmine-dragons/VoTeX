6a85866979105dc38a3d6dcbac472e55a921283b
Nishant <nishantb1130@gmail.com>
2021-03-27 11:38:08 -0700

    rename

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { LeftPanel } from './components/leftPanel.component'
+
+function App() {
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default App;
